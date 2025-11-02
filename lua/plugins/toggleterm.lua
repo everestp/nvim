0a777e636fcbf8d9140e0902d8e21a1d9f39664c
everestp <paudeleverest09@gmail.com>
2025-11-02 15:18:40 +0545

    GFG

---
+++ lua/plugins/toggleterm.lua
@@ -0,0 +1,25 @@
+-- lua/plugins/toggleterm.lua
+return {
+  "akinsho/toggleterm.nvim",
+  version = "*",
+  config = function()
+    require("toggleterm").setup{
+      size = 20,
+      open_mapping = [[<leader>tt]], -- optional, can also map in keymaps.lua
+      hide_numbers = true,
+      shade_filetypes = {},
+      shade_terminals = true,
+      shading_factor = 2,
+      start_in_insert = true,
+      insert_mappings = true,
+      persist_size = true,
+      direction = "float",
+      close_on_exit = true,
+      float_opts = {
+        border = "curved",
+        winblend = 0,
+        highlights = { border = "Normal", background = "Normal" },
+      }
+    }
+  end
+}
